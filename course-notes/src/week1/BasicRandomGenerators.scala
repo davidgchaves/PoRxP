2eb53e2fb272680bfabdcd78b779b100d207db87
David G Chaves <davidgchaves@gmail.com>
2015-04-19 22:24:13 +0200

    Add a Generator type

--- course-notes/src/week1/BasicRandomGenerators.scala
+++ course-notes/src/week1/BasicRandomGenerators.scala
@@ -1,4 +1,19 @@
 object BasicRandomGenerators {
   println("Welcome to the Basic Random Generators' Notes")
 
+  // The Generator 'type'
+  trait Generator[+T] {
+    self =>    // an alias for 'this'
+
+    def generate: T
+
+    def map[S](f:T => S): Generator[S] = new Generator[S] {
+      def generate = f(self.generate)
+    }
+
+    def flatMap[S](f:T => Generator[S]): Generator[S] = new Generator[S] {
+      def generate = f(self.generate).generate
+    }
+  }
+
 }
