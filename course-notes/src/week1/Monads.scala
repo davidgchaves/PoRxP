1b7f0957dbaf5ed438e21eb168e09945c42541bb
David G Chaves <davidgchaves@gmail.com>
2015-04-20 13:09:34 +0200

    Add Monads Notebook
    A Scala Worksheet (.sc) changed to .scala file

---
+++ course-notes/src/week1/Monads.scala
@@ -0,0 +1,13 @@
+/*
+  Instructions to convert this file into a Worksheet file:
+    1: Change file extension from .scala to .sc
+    2: Change object name to something else
+       (like MonadsWorksheet)
+*/
+package week1
+
+object Monads {
+  println("Welcome to the Monads' Notes")
+
+}
+
